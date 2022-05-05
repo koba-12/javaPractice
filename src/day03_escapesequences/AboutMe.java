2bbe4f9224423071b9dc1e6b94d41c5e36b9a3b3
koba-12 <cuneytyilmaaz@gmail.com>
2022-05-05 19:35:59 +0300

    day44 Dog_2.java

---
+++ src/day03_escapesequences/AboutMe.java
@@ -0,0 +1,12 @@
+package day03_escapesequences;
+
+public class AboutMe {
+
+    public static void main(String[] args) {
+
+        System.out.println("My favorite TV show is\"Lost\"");
+        System.out.println("My favorite is \"Unutulanlar\"");
+        System.out.println("My favorite quote is\".....\"");
+        System.out.println("My favorite book is\"the LOTR\"");
+    }
+}

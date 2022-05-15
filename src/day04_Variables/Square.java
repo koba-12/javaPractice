92b97ca94014c0fa9bcf2f9c7b7b86afd1fd8bd0
koba-12 <100583019+koba-12@users.noreply.github.com>
2022-05-15 14:58:45 +0300

    Add files via upload

---
+++ src/day04_Variables/Square.java
@@ -0,0 +1,20 @@
+package day04_Variables;
+
+public class Square {
+
+    public static void main(String[] args) {
+
+        //data type variable name=data
+        // sevnura sor math işlemlerini anlamadım
+        //bunlar ölçü birimi bunlarda double kullanılıyor.
+
+        double side = 8.5;
+        double area = side * side;
+        double perimeter = side * 4;
+
+        System.out.println("side = " + side);
+        System.out.println("area = " + area);
+        System.out.println("perimeter = " + perimeter);
+
+    }
+}

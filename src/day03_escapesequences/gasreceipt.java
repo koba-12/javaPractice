2059fd7b5c9284f4ba4385d59d6da1c3dfb7057c
koba-12 <cuneytyilmaaz@gmail.com>
2022-05-15 14:57:28 +0300

    gasreceipt.javajava

---
+++ src/day03_escapesequences/gasreceipt.java
@@ -0,0 +1,20 @@
+package day03_escapesequences;
+
+public class gasreceipt {
+
+    public static void main(String[] args) {
+
+        System.out.println();
+        System.out.println("    MCLEAN STORE");
+        System.out.println();
+        System.out.println("09/20/21  09:25PM");
+        System.out.println();
+        System.out.println("Gallons:      11.848");
+        System.out.println("Price/Gallon: $2.089");
+        System.out.println();
+        System.out.println("Fuel Total    $24.734");
+
+    }
+}
+
+
